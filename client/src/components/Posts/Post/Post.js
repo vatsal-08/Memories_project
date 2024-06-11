d08a4ccb69bd000d870e4741e4532589c337a153
Vatsal Khatri <201951081@iiitvadodara.ac.in>
2024-06-11 23:28:49 +0530

    Adding styles component and many icons in Post.js

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,6 +1,73 @@
 import React from "react";
-import { styles } from "./styles.js";
-const Post = () => {
-  return <h1>POST</h1>;
+import styles from "./styles.js";
+import {
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Button,
+  Typography,
+} from "@mui/material";
+import moment from "moment";
+import ThumbUpAltIcon from "@mui/icons-material/ThumbUpAlt";
+import DeleteIcon from "@mui/icons-material/Delete";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+const Post = ({ post }) => {
+  return (
+    <Card className={styles.card}>
+      <CardMedia
+        className={styles.media}
+        image={
+          post.selectedFile ||
+          "https://user-images.githubusercontent.com/194400/49531010-48dad180-f8b1-11e8-8d89-1e61320e1d82.png"
+        }
+        title={post.title}
+      />
+      <div className={styles.overlay}>
+        <Typography variant="h6">{post.creator}</Typography>
+        <Typography variant="body2">
+          {moment(post.createdAt).fromNow()}
+        </Typography>
+      </div>
+      <div className={styles.overlay2}>
+        <Button style={{ color: "white" }} size="small" onClick={() => {}}>
+          <MoreHorizIcon fontSize="default" />
+        </Button>
+      </div>
+      <div className={styles.details}>
+        <Typography variant="body2" color="textSecondary">
+          {post.tags?.map((tag) => `#${tag}`)}
+        </Typography>
+      </div>
+      <Typography
+        className={styles.title}
+        gutterBottom
+        variant="h5"
+        component="h2"
+      >
+        {post.title}
+      </Typography>
+      <CardContent>
+        <Typography
+          className={styles.title}
+          color="textSecondary"
+          component="p"
+        >
+          {post.message}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small" color="primary" variant="h5" onClick={() => {}}>
+          <ThumbUpAltIcon fontSize="small" />
+          Like
+          {post.likeCount}
+        </Button>
+        <Button size="small" color="primary" variant="h5" onClick={() => {}}>
+          <DeleteIcon fontSize="small" />
+          Delete
+        </Button>
+      </CardActions>
+    </Card>
+  );
 };
 export default Post;
